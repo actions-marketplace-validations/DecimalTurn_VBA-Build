ff910429970e33d6310748bda0c10f72f1fcef4f
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2025-03-28 07:52:23 +0000

    Improve error reporting

--- Enable-Vbom.ps1
+++ Enable-Vbom.ps1
@@ -1,13 +1,14 @@
-
-
 function Enable-VBOM ($App) {
   Try {
     $CurVer = Get-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\$App.Application\CurVer -ErrorAction Stop
     $Version = $CurVer.'(default)'.replace("$App.Application.", "") + ".0"
 
     Set-ItemProperty -Path HKCU:\Software\Microsoft\Office\$Version\$App\Security -Name AccessVBOM -Value 1 -ErrorAction Stop
+    Write-Output "Successfully enabled access to VBA project object model for $App."
   } Catch {
     Write-Output "Failed to enable access to VBA project object model for $App."
+    Write-Output "Error: $($_.Exception.Message)"
+    Write-Output "StackTrace: $($_.Exception.StackTrace)"
   }
 }
 
