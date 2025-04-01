a083ef853b4a4032bf190c17168ac10c372e50c3
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2025-04-01 14:43:02 -0400

    Update Main.ps1
    Use PS Script Root

--- Main.ps1
+++ Main.ps1
@@ -1,12 +1,16 @@
 Write-Host "Current directory: $(pwd)"
+
 Write-Host "Changing zip file name and location"
-. ./Zip-It.ps1
+. "$PSScriptRoot\Zip-It.ps1"
 Write-Host "========================="
+
 Write-Host "Closing Office applications"
-. ./Close-Office.ps1
+. "$PSScriptRoot\Close-Office.ps1"
 Write-Host "========================="
+
 Write-Host "Enabling VBOM (Visual Basic for Applications Object Model)"
-. ./Enable-VBOM.ps1
+. "$PSScriptRoot\Enable-VBOM.ps1"
 Write-Host "========================="
+
 Write-Host "Importing VBA code into Office document"
-. ./Build-VBA.ps1
+. "$PSScriptRoot\Build-VBA.ps1"
