0a83b143e839ff12eb3f6f88330e3b9264a5fe36
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2025-04-01 17:40:19 +0000

    Use proper capitalization for directories (XMLsource and XMLoutput)

--- Zip-It.ps1
+++ Zip-It.ps1
@@ -1,12 +1,12 @@
 # This is a a powershell script that used 7zip to compress files and folders
-# The files and folders to compress are located in the src/XMLSource folder
+# The files and folders to compress are located in the src/XMLsource folder
 # It contains files such as src/skeleton.xlsm/XMLsource/[Content_Types].xml and folders such as src/skeleton.xlsm/XMLsource/_rels
 # The script will compress the files and folders into a zip file located in the src/skeleton.xlsm/XMLsource folder
 # The zip file will be named skeleton.zip
 # The script will use 7zip to compress the files and folders
 # The script will use the 7zip command line interface to compress the files and folders
 
-# This script uses 7-Zip to compress files and folders in the src/XMLSource directory into a zip file named skeleton.zip.
+# This script uses 7-Zip to compress files and folders in the src/XMLsource directory into a zip file named skeleton.zip.
 
 Write-Host "Staring the compression process..."
 
@@ -14,8 +14,8 @@ $currentDir = Get-Location
 Write-Host "Current directory: $currentDir"
 
 # Define the source folder and the output zip file
-$sourceFolder = "src/skeleton.xlsm/XMLSource/"
-$outputZipFile = "src/skeleton.xlsm/XMLOutput/skeleton.zip"
+$sourceFolder = "src/skeleton.xlsm/XMLsource/"
+$outputZipFile = "src/skeleton.xlsm/XMLoutput/skeleton.zip"
 
 # Path to the 7-Zip executable
 $sevenZipPath = "7z"  # Assumes 7-Zip is in the system PATH. Adjust if necessary.
@@ -85,8 +85,8 @@ if ($LASTEXITCODE -ne 0) {
 Write-Host "Compression completed successfully. Zip file created at: $absoluteDestinationFolder"
 
 
-# Create a copy of the zip file in the src/skeleton.xlsm/XMLOutput folder at the /src level
-$copySource = "src/skeleton.xlsm/XMLOutput/skeleton.zip"
+# Create a copy of the zip file in the src/skeleton.xlsm/XMLoutput folder at the /src level
+$copySource = "src/skeleton.xlsm/XMLoutput/skeleton.zip"
 $renameDestination = "./skeleton.xlsm"
 
 # Delete the destination file if it exists
